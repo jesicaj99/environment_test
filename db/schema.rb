ee8fc885e9691e6e3ecf2c3a29780e1ca885a31e
root <root@localhost>
2022-09-15 22:11:51 +0000

    correcting integration and unit test

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_160258) do
+ActiveRecord::Schema.define(version: 2022_09_15_212548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.decimal "price"
     t.date "published"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'JK rowling'
@@ -13,8 +13,45 @@ RSpec.describe 'Creating a book', type: :feature do
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK rowling'
+    fill_in 'Price', with: '123'
+    select '2020', :from => 'book_published_1i'
+    select 'November', :from => 'book_published_2i'
+    select '2', :from => 'book_published_3i'
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content('JK rowling')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK rowling'
+    fill_in 'Price', with: '123'
+    select '2020', :from => 'book_published_1i'
+    select 'November', :from => 'book_published_2i'
+    select '2', :from => 'book_published_3i'
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content('123')
+  end
+
+  scenario 'valid published' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK rowling'
+    fill_in 'Price', with: '123'
+    select '2020', :from => 'book_published_1i'
+    select 'November', :from => 'book_published_2i'
+    select '2', :from => 'book_published_3i'
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content('2020-11-02')
   end
+  
 end
